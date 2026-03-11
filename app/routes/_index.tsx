dcba8122097aa5ba670f9808ef387c20d6811e39
LczFerreira <165179582+LczFerreira@users.noreply.github.com>
2026-03-11 18:43:25 -0300

    Update _index.tsx

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -14,6 +14,7 @@ export default function Index() {
   return (
     <div className="flex flex-col h-full w-full">
       <Header />
+      
       <ClientOnly fallback={<BaseChat />}>{() => <Chat />}</ClientOnly>
     </div>
   );
